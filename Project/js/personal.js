3f52770b1f952506c1c8829b59d6d93b6da73016
Kenny <k603062684@gmail.com>
2022-05-03 02:37:48 +0800

    範例網頁demo完成

--- Project/js/personal.js
+++ Project/js/personal.js
@@ -14,5 +14,32 @@ window.onscroll = () => {
     header.classList.remove("active");
     document.body.classList.remove("active");
   }
-  console.log("hi");
+  document.querySelectorAll("section").forEach((sec) => {
+    let top = window.scrollY;
+    //從頂部開始計算已滾動的垂直方向的像素值
+    let offset = sec.offsetTop - 150;
+    //該section距離父元素頂端的的距離(Y軸px)
+    //-150是為了讓瀏覽器提早辨識使用者已經進入該section的區域了
+    //有時候滾輪的幅度比較大，造成一個滑動已經過頭，早就進入該section了
+    let height = sec.offsetHeight;
+    //該section的高度(含邊框)
+    let id = sec.getAttribute("id");
+    console.log(id);
+    //取得各section的id
+    if (top >= offset && top < offset + height) {
+      //當畫面滾動到進入該section的區域時(已提早150px)
+      document.querySelectorAll(".header .navbar a").forEach((links) => {
+        //選擇所有navbar裡的a標籤
+        console.log(links);
+        links.classList.remove("active");
+        //移除active class
+        document
+          .querySelector(".header .navbar a[href*=" + id + "]")
+          //*=屬性選擇器不加引號也可以?
+          .classList.add("active");
+        // let a = document.querySelector(".header .navbar a[href*='" + id + "']");
+        // console.log(a);
+      });
+    }
+  });
 };
