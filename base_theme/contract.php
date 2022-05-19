bd43aef1f4defd18b6c94055c9fb696be5740bae
Kenny <k603062684@gmail.com>
2022-05-20 07:31:15 +0800

    5/20

---
+++ base_theme/contract.php
@@ -0,0 +1,16 @@
+<?php include_once("php/head.php"); ?>
+<div class="page_contract_topimg">
+    <h1>Contract us</h1>
+</div>
+
+<div class="page_contract_box">
+    <h1>聯絡我們</h1>
+    <img src="./img/logo2.png" alt="">
+    <p>若您有任何問題或意見，歡迎填寫表單給我們，謝謝</p>
+    <div>
+        <div><img src="./img/LINE_ALBUM_茶葉桌巾形象照_211229_11.jpg" alt=""></div>
+        <div></div>
+    </div>
+</div>
+
+<?php include_once("php/foot.php"); ?>
--- base_theme/place.php
+++ base_theme/place.php
@@ -1,4 +1,38 @@
 <?php include_once("php/head.php"); ?>
+<div class="page_place_topimg">
+    <h1>Sales office</h1>
+</div>
 
+<div class="page_place_box">
+    <h1>銷售據點</h1>
+    <img src="./img/logo2.png" alt="">
+    <div class="places">
+        <div class="frt_place">
+            <h2>台中新光館</h2>
+            <hr>
+            <div>
+                <i class="fa-solid fa-phone"></i>
+                <p>04 2250 0582</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-house"></i>
+                <p>407台中市西屯區台灣大道三段101號7樓 <br>(台中新光三越中港店7樓)</p>
+            </div>
+        </div>
+        <div class="sec_place">
+            <h2>鹿港店</h2>
+            <hr>
+            <div>
+                <i class="fa-solid fa-phone"></i>
+                <p>04 2250 0582</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-house"></i>
+                <p>505彰化縣鹿港鎮文開路32巷1號<br>(鹿港老街)</p>
+            </div>
+        </div>
+    </div>
+
+</div>
 
 <?php include_once("php/foot.php"); ?>
